57288721f024c7a4d16059aca87e07146b1054e3
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2023-01-15 21:17:27 +0900

    debug

--- staging/main.tf
+++ staging/main.tf
@@ -16,8 +16,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
+  // access_key = var.aws_access_key
+  // secret_key = var.aws_secret_key
   region     = "ap-northeast-1"
 }
 
