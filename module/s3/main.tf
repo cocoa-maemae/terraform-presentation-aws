84f2c773e5aa1fbf5d89c3b29075590ade18611c
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2023-01-21 18:20:48 +0900

    fix bucket name

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -1,7 +1,11 @@
+locals {
+  env = substr("${terraform.workspace}", 26)
+}
+
 resource "aws_s3_bucket" "example" {
-  bucket = "example-bucket-substr(${terraform.workspace}, 26)"
+  bucket = "example-bucket-${local.env}"
 
   tags = {
-    Name        = "example bucket"
+    Name = "example bucket"
   }
 }
