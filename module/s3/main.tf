5315e576ebe6bac78aa41a672f8ecf871dc653af
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2023-01-23 00:31:17 +0900

    fix acl

--- module/s3/main.tf
+++ module/s3/main.tf
@@ -2,11 +2,15 @@ locals {
   env = substr("${terraform.workspace}", 27, -1)
 }
 
-resource "aws_s3_bucket" "example" {
+resource "aws_s3_bucket" "example_bucket" {
   bucket = "example-bucket-${local.env}"
-  acl    = "public-read"
 
   tags = {
     Name = "example bucket for ${local.env}"
   }
+}
+
+resource "aws_s3_bucket_acl" "example_bucket_acl" {
+  bucket = aws_s3_bucket.example_bucket.id
+  acl    = "public-read"
 }
