937e9eecb05e55a8a259a8ca228acf455d5554e9
cocoa-maemae <breezin_ahimusa@yahoo.co.jp>
2023-01-23 00:41:03 +0900

    add sqs

--- module/sqs/main.tf
+++ module/sqs/main.tf
@@ -1,3 +1,11 @@
 locals {
   env = substr("${terraform.workspace}", 27, -1)
 }
+
+resource "aws_sqs_queue" "example"_sqs {
+  name                      = "example-queue-${local.env}"
+  delay_seconds             = 60
+  max_message_size          = 2048
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+}
